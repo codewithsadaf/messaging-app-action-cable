f15b603044dfce71b436477137f9ae6345dd942c
Sadaf Arshad <sadafdev89@gmail.com>
2024-05-21 18:55:14 +0500

    fixed design work

--- app/javascript/controllers/scroll_controller.js
+++ app/javascript/controllers/scroll_controller.js
@@ -1,10 +1,30 @@
 import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect(){
-    console.log("Connected scroll");
-    const messages = document.getElementById("messages");
-    messages.addEventListener("DOMNodeInserted", this.resetScroll);
-    this.resetScroll(messages);
+    const messagesDiv = document.getElementById('messages');
+
+    function loadOlderMessages() {
+      messagesDiv.scrollTop = olderMessages.scrollHeight;
+    }
+
+    messagesDiv.addEventListener('scroll', () => {
+      if (messagesDiv.scrollTop === 0) {
+        loadOlderMessages();
+      }
+    });
+
+    window.addEventListener('load', () => {
+      messagesDiv.scrollTop = messagesDiv.scrollHeight;
+    });
+
+    function scrollToBottom() {
+      messagesDiv.scrollTop = messagesDiv.scrollHeight;
+    }
+
+    const observer = new MutationObserver(scrollToBottom);
+    observer.observe(messagesDiv, { childList: true });
+
+    scrollToBottom();
   }
   resetScroll(){
     const bottomOfScroll = messages.scrollHeight - messages.clientHeight;
